ff737bf7ba2cec32ae410aa81fcdc25df7a44325
Ismael <ismael.codemegsoft@gmail.com>
2021-07-17 17:48:08 +0530

    local chnage

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,13 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('role_id')->comment('1 for admin, 2 for user')->unique()->default(2);;
+            $table->bigInteger('phone');
+            $table->string('profile_image')->default(NULL);
+            $table->tinyInteger('is_active')->comment('0 for deactivate, 1 for active')->default(1);
+            $table->tinyInteger('device_type')->comment('1 for android, 2 for ios')->default(0);
+            $table->string('device_token');
+            $table->string('address')->default(NULL);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
